02675e710cee51faf7e4b7fc5d4093dcf2c89b31
HexCharger <zelazows@student.agh.edu.pl>
2016-04-29 12:14:25 +0200

    some transitions

--- powergrid_pdf.m
+++ powergrid_pdf.m
@@ -1,16 +1,25 @@
-% Example-29-1: SUMMARY: token selection from a single input place
+% Simulation of Powergrid
 
 function [png] = powergrid_pdf()
 
 png.PN_name = 'Powergrid Simulation';
 png.set_of_Ps = {'JNW','RBL','ZAG','KDN','ANI','GUA','WRZ','BLE','SIK','KAW','KLP',...
-                 'KLD','PAN','PRA','OLE','SDW','DBR','LBN','HER','KSC','BKW','KLT',...
+                 'KLO','PAN','PRA','OLE','SOW','DBR','LBN','HER','KSC','BKW','KLT',...
                  'ZWA','SEC','KOT','KNC','JLK','POH','MYL','MIJ','PRJ','RUD','RED',...
                  'CYK','HCW'};
 
-png.set_of_Ts = {'tColor', 'tA', 'tA_or_B', 'tA_or_B_Preferred','tA_and_B'};
+png.set_of_Ts = {...
+    'tGEN_3','tGEN_4','tGEN_AT1',...
+    'tKDN_2_ZAG','tZAG_2_KLO','tKLO_2_PAN','tPAN_2_PRA','tPRA_2_OLE','tOLE_2_SOW','tSOW_2_DBR',...
+    
+};
 
 png.set_of_As = {...
+%    'GEN_3',?,1,    ... % Generator 3 empty transition
+%    'GEN_4',?,1,    ... % Generator 4 empty transition
+    
+
+
     'pStart','tColor',1, 'tColor','pCombined',1,   ... % tColor
     'pCombined','tA',1, 'tA','pA',1 ...                % tA
     'pCombined','tA_or_B',1, 'tA_or_B','pA_or_B',1 ... % tA_or_B
