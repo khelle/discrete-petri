d2ca88ddafa4bc57badff03145913b3d1e2426f1
mtraton <mtraton@studenty.agh.edu.pl>
2016-04-29 11:24:31 +0200

    adding first batch of places

--- powergrid_pdf.m
+++ powergrid_pdf.m
@@ -3,8 +3,9 @@
 function [png] = powergrid_pdf()
 
 png.PN_name = 'Ex-29-1: token selection from a single input place';
-png.set_of_Ps = {'pStart', 'pCombined', 'pA', 'pA_or_B', ...
-                 'pA_or_B_Preferred','pA_and_B'};
+png.set_of_Ps = {'ZWA', 'SEC', 'KOT', 'KNC', 'JLK', 'POH' ...  
+    'MYL', 'MIJ', 'PRJ', 'RUD', 'RED', 'CYK', 'ANI', 'HCW' 'GUA' ... 
+        'WRZ'};
              
 png.set_of_Ts = {'tColor', 'tA', 'tA_or_B', 'tA_or_B_Preferred','tA_and_B'};
 
